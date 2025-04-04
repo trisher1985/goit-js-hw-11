2151551d0fe3605940f3670c56046280461f47d2
trisher1985 <106806921+trisher1985@users.noreply.github.com>
2025-04-04 13:07:04 +0300

    Initial commit

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,39 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const inputDelay = form.elements.delay;
+const radioBtn = form.elements.state;
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const delay = inputDelay.value;
+  const state = radioBtn.value;
+
+  const prom = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  prom
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
